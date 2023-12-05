d310ca57906e73b2ad9c3fcf4accec2f2993e8ee
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-04 21:45:05 -0600

    Cart Item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,33 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
+import PropTypes from 'prop-types';
+import { ChevronUp, ChevronDown } from '../icons';
+
+const CartItem = ({ img, title, price, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+CartItem.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.string,
+  amount: PropTypes.number,
 };
 
 export default CartItem;
