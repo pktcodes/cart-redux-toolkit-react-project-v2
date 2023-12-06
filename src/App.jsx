2076eda3f88b00d44ba1d0a8992f5be275b75dd5
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-05 20:08:31 -0600

    Modal Setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const cartItems = useSelector((state) => state.cart.cartItems);
@@ -18,6 +19,7 @@ function App() {
     <main>
       <Navbar />
       <CartContainer />
+      <Modal />
     </main>
   );
 }
